b23e245f29bf60e18cb28631c876a5a08a05999c
Herick Filipe <klannlimaherickfilipe@gmail.com>
2026-05-12 16:54:47 -0300

    alteraco do banco de dados

--- Ecommerce/ecommerce1/database/migrations/2026_05_11_192829_create_movimentos_table.php
+++ Ecommerce/ecommerce1/database/migrations/2026_05_11_192829_create_movimentos_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('movimentos', function (Blueprint $table) {
             $table->id();
+            $table-> foreignId('produto_id')->constraint() ->casacdeonDelete();
+            //migrations que cria uma foreing key com comportamento automaticode exclusao em casacata
+            //Exclusão em cascata: se um registro da tabela produtos for excluido, todos
+            //os registros da tabela atual que referenciam esse produto_id tambem serão
+            //apagados automaticamente
+            $table->integer('quantidade');
+            $table->enum('tipo', ['entrada', 'saida']);
+
             $table->timestamps();
         });
     }
